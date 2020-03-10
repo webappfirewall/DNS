7fc611fad78a71dd8043d58b72d1cca08e7ecc19
Jason Rossello <jrossellor1400@alumno.ipn.mx>
2020-03-09 23:16:17 -0600

    New bind

--- dnsd.py
+++ dnsd.py
@@ -2,8 +2,6 @@ import socket
 import urllib.parse
 from pymongo import MongoClient
 
-ip = "127.0.0.1"
-port = 53
 id = ''
 flags = ''
 quest = ''
@@ -16,38 +14,53 @@ type = ''
 query_class = ''
 
 username = urllib.parse.quote_plus('@dm1n')
-passwor = urllib.parse.quote_plus('Qw3rt&.12345')
-client = MongoClient('mongodb://%s:%s@10.0.2.4' % (username, passwor))
+password = urllib.parse.quote_plus('Qw3rt&.12345')
+client = MongoClient('mongodb://%s:%s@10.0.2.4' % (username, password))
 db = client['waf']
+collection = db['sites']
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as socket_udp:
-	socket_udp.bind((ip, port))
+    socket_udp.bind(("52.191.249.108", 53))
+    print("DNS Server by waf")
 
-	while True:
-		data, address = socket_udp.recvfrom(1024)
+    while True:
+        data, address = socket_udp.recvfrom(1024)
+        id = data[:2].hex()
+        print("id: ", id)
+        flags = data[2:4].hex()
+        print("flags: ", flags)
+        quest = data[4:6].hex()
+        print("quest: ", quest)
+        ans = data[6:8].hex()
+        print("ans: ", ans)
+        auth = data[8:10].hex()
+        print("auth: ", auth)
+        add = data[10:12].hex()
+        print("add: ", add)
+        query = data[12:]
+        print("query: ", query)
+        type = query[-1]
+        print("type: ", type)
+        query_class = query[-3]
+        print("query_class: ", query_class)
 
-		id = int(data[:2].hex(), 16)
-		flags =  int(data[2:4].hex(), 16)
-		quest = int(data[4:6].hex(), 16)
-		ans = int(data[6:8].hex(), 16)
-		auth = int(data[8:10].hex(), 16)
-		add = int(data[10:12].hex(), 16)
-		query = data[12:]
-		type = query[-1]
-		query_class = query[-3]
+        i = 0
+        for c in query:
+            if i == 0:
+                pos = c + 1
+            elif i == pos:
+                url += '.'
+                pos = c + i + 1
+            elif c == 0:
+                break
+            elif chr(c).isalpha():
+                url += chr(c)
+            i += 1
+        url = url[:-3]
 
-		i = 0
+        doc = collection.find_one({"url": url})
 
-		for c in query:
-			if i == 0:
-				pos = c + 1
-			elif i == pos:
-				url += '.'
-				pos = c + i + 1
-			elif c == 0:
-				break
-			elif chr(c).isalpha():
-				url += chr(c)
-			i += 1
-		print(url[:-3])
-		url = ''
+        if doc is not None:
+            print(doc['ip'])
+        print(url)
+        url = ''
