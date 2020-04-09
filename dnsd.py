f9d15acd51c661ab3ebe93d2c409860b04d4429b
Jason Rossello <jrossellor1400@alumno.ipn.mx>
2020-04-09 11:43:27 -0500

    dns v1.1.1

--- dnsd.py
+++ dnsd.py
@@ -4,62 +4,67 @@ import threading
 from pymongo import MongoClient
 
 
-def analyzeRqt(s_udp, data, addr):
-    usr = urllib.parse.quote_plus('@dm1n')
-    pwd = urllib.parse.quote_plus('Qw3rt&.12345')
-    client = MongoClient('mongodb://%s:%s@192.168.17.146' % (usr, pwd))
-    # client = MongoClient('mongodb://localhost:27017')
-    db = client['waf']
-    collection = db['sites']
+def analyzeRqt(conn, addr):
+    with conn:
+        data = conn.recv(1024)
 
-    response = bytearray()
+        usr = urllib.parse.quote_plus('@dm1n')
+        pwd = urllib.parse.quote_plus('Qw3rt&.12345')
+        client = MongoClient('mongodb://%s:%s@192.168.17.146' % (usr, pwd))
+        # client = MongoClient('mongodb://localhost:27017')
+        db = client['waf']
+        collection = db['sites']
 
-    id = data[:2]
-    query = data[12:]
-
-    url = extractURL(query)
-    print('URL: ', url)
-
-    doc = collection.find_one({"url": url})
-
-    if doc is not None:
-        for i in id:
-            response.append(i)
-        response.append(129)
-        response.append(128)
-        response.append(0)
-        response.append(1)
-        response.append(0)
-        response.append(1)
-        response.append(0)
-        response.append(0)
-        response.append(0)
-        response.append(0)
-        for i in query:
-            response.append(i)
-        response.append(192)
-        response.append(12)
-        response.append(0)
-        response.append(1)
-        response.append(0)
-        response.append(1)
-        response.append(0)
-        response.append(0)
-        response.append(0)
-        response.append(255)
-        response.append(0)
-        response.append(4)
-        response.append(192)
-        response.append(168)
-        response.append(17)
-        response.append(149)
-        s_udp.sendto(response, addr)
         response = bytearray()
-    else:
-        s_udp.sendto(data, ('8.8.8.8', 53))
-        data2, addr2 = s_udp.recvfrom(1024)
-        s_udp.sendto(data2, addr)
-    url = ''
+
+        id = data[:2]
+        query = data[12:]
+
+        url = extractURL(query)
+        print('URL: ', url)
+
+        doc = collection.find_one({"url": url})
+
+        if doc is not None:
+            for i in id:
+                response.append(i)
+            response.append(129)
+            response.append(128)
+            response.append(0)
+            response.append(1)
+            response.append(0)
+            response.append(1)
+            response.append(0)
+            response.append(0)
+            response.append(0)
+            response.append(0)
+            for i in query:
+                response.append(i)
+            response.append(192)
+            response.append(12)
+            response.append(0)
+            response.append(1)
+            response.append(0)
+            response.append(1)
+            response.append(0)
+            response.append(0)
+            response.append(0)
+            response.append(255)
+            response.append(0)
+            response.append(4)
+            response.append(192)
+            response.append(168)
+            response.append(17)
+            response.append(149)
+            conn.sendall(response)
+            response = bytearray()
+        else:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s_tcp2:
+                s_tcp2.connect(('8.8.8.8', 53))
+                s_tcp2.sendall(data)
+                data2 = s_tcp2.recv(1024)
+                conn.sendall(data2)
+        url = ''
 
 
 def extractURL(query):
@@ -82,16 +87,16 @@ def extractURL(query):
 
 
 def initDNS():
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s_udp:
-        s_udp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s_udp.bind(('192.168.17.147', 53))
-        print("***** DNS Server *****")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s_tcp:
+        s_tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s_tcp.bind(('192.168.17.147', 53))
+        s_tcp.listen()
 
+        print("***** DNS Server *****")
         while True:
-            data, addr = s_udp.recvfrom(1024)
-            t = threading.Thread(target=analyzeRqt, args=(s_udp, data, addr))
+            conn, addr = s_tcp.accept()
+            t = threading.Thread(target=analyzeRqt, args=(conn, addr))
             t.start()
-            t.join()
 
 
 if __name__ == '__main__':
