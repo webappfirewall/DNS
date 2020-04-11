cc0660844e05912a19dea562892cc415451951f2
Jason Rossello <jrossellor1400@alumno.ipn.mx>
2020-04-11 15:37:06 -0500

    dns v1.2.5

--- dnsd.py
+++ dnsd.py
@@ -27,7 +27,9 @@ def analyzeRqt(conn, addr):
         if doc is not None:
             response = bytearray()
             length = bytearray(b'\x00')
-            length.append(len(b1) + len(b2) + len(query) + 4)
+            size = len(b1) + len(b2) + len(query) + 4
+            print(size)
+            length.append(size)
 
             ip = doc['ip'].split('.')
 
